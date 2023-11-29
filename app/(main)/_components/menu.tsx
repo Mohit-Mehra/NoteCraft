0e4be9c7b554e38b6bf81965ca346a1bc3e7e1ef
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-11-29 21:05:03 +0530

    banner component

---
+++ app/(main)/_components/menu.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Id } from "@/convex/_generated/dataModel";
+
+interface MenuProps {
+  documentId: Id<"documents">;
+}
+
+export const Menu = ({ documentId }: MenuProps) => {
+  return <div>Menu</div>;
+};
